4e2f0863ff6a31913eec1afdff9d92a7bc229e84
Jurgens Smit <contact@jurgenssmit.com>
2013-06-03 18:34:45 +1000

    add avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,27 +1,35 @@
 require_relative '../../lib/github'
 
 class User
-  attr_reader :username
-  attr_reader :total_score
+  attr_reader :username, :total_score, :avatar_url
+
   def self.find(username)
-  User.new(username)
-end
+    User.new(username)
+  end
 
   def calculate
     @total_score = scorer.total_score
+    @avatar_url = GithubUser.new(@username, data).avatar_url
     self
   end
 
   private
+
   def initialize(username)
     @username = username
   end
 
   def scorer
     unless @scorer
-      data = Github::Event.new(@username).get
       @scorer = GithubScorer.new(data).score
     end
     @scorer
+  end
+
+  def data
+    unless @data
+      @data = Github::Event.new(@username).get
+    end
+    @data
   end
 end
